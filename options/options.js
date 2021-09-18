c6cbe147a1d9073187e086bc379fddc1c64ffbe6
Crowdbotics <team@crowdbotics.com>
2021-09-18 06:29:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "nameless_feather_30655Identifier",
+  url: "https://nameless_feather_30655Identifier.botics.co",
+  api: "https://nameless_feather_30655Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
